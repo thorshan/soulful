b3cc8be6311b98dfc4a2506417d684b81cff6af8
thorshan23 <thorshan23@gmail.com>
2025-10-14 05:27:36 +0000

    FIX

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,9 +2,8 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const auth = require('../middleware/auth');
-const role = require('../middleware/role');
 
-router.post('/register', auth, role(['admin']), authController.register);
+router.post('/register', authController.register);
 router.post('/login', authController.login);
 router.post('/logout', auth, authController.logout);
 
