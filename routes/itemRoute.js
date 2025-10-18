50faa44dfeebff8a9f76fe37493d09ffc969a23d
thorshan23 <thorshan23@gmail.com>
2025-10-18 10:06:08 +0000

    FIX_ITEM

--- routes/itemRoute.js
+++ routes/itemRoute.js
@@ -4,9 +4,9 @@ const itemController = require('../controllers/itemController');
 const auth = require('../middleware/auth');
 const role = require('../middleware/role');
 
-router.get('/items', auth, role(["admin", "moderator"]), itemController.getAllItems);
+router.get('/items', itemController.getAllItems);
 router.post('/items', auth, role(["admin", "moderator"]), itemController.createItem);
-router.get('/items/:id', auth, role(["admin", "moderator"]), itemController.getItem);
+router.get('/items/:id', itemController.getItem);
 router.put('/items/:id', auth, role(["admin", "moderator"]), itemController.updateItem);
 router.delete('/items/:id', auth, role(["admin", "moderator"]), itemController.deleteItem);
 
