a20f6526979638a4ef37931afa9ce0fa5afa0b85
thorshan23 <thorshan23@gmail.com>
2025-10-20 06:18:55 +0000

    FIX_AUTH

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -4,9 +4,9 @@ const userController = require("../controllers/userController");
 const auth = require('../middleware/auth');
 const role = require('../middleware/role');
 
-router.get("/users", auth, role(["admin"]), userController.allUsers);
+router.get("/users", auth, role(["admin", "moderator"]), userController.allUsers);
 router.post("/users", auth, role(["admin"]), userController.createUser);
-router.get("/users/:id", auth, role(["admin"]), userController.getUser);
+router.get("/users/:id", auth, role(["admin", "moderator", "user"]), userController.getUser);
 router.put("/users/:id", auth, role(["admin"]), userController.updateUser);
 router.delete("/users/:id", auth, role(["admin"]), userController.deleteUser);
 
