ccd881300c15d071baf5746cb2267b1180e6307d
thorshan23 <thorshan23@gmail.com>
2025-10-20 10:40:04 +0000

    FIX

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -5,7 +5,7 @@ const auth = require("../middleware/auth");
 const role = require("../middleware/role");
 
 router.get("/reviews", auth, role(["admin", "moderator"]), reviewController.getAllReviews);
-router.post("/reviews", auth, role(["admin", "moderator", , "user"]), reviewController.getAllReviews);
+router.post("/reviews", auth, role(["admin", "moderator", "user"]), reviewController.getAllReviews);
 router.get("/reviews/:id", reviewController.getAllReviews);
 router.delete("/reviews/:id", auth, role(["admin", "moderator"]), reviewController.getAllReviews);
 
