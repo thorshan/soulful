c5fc3e3429156b6dfc912f240f706e7bb7f9a511
thorshan23 <thorshan23@gmail.com>
2025-10-20 10:31:45 +0000

    FIX

--- routes/reviewRoute.js
+++ routes/reviewRoute.js
@@ -4,9 +4,9 @@ const reviewController = require("../controllers/reviewController");
 const auth = require("../middleware/auth");
 const role = require("../middleware/role");
 
-router.get("/reviews", reviewController.getAllReviews);
-router.post("/reviews", reviewController.getAllReviews);
-router.get("/reviews/:id", auth, role(["admin", "moderator"]), reviewController.getAllReviews);
+router.get("/reviews", auth, role(["admin", "moderator"]), reviewController.getAllReviews);
+router.post("/reviews", auth, role(["admin", "moderator", , "user"]), reviewController.getAllReviews);
+router.get("/reviews/:id", reviewController.getAllReviews);
 router.delete("/reviews/:id", auth, role(["admin", "moderator"]), reviewController.getAllReviews);
 
 module.exports = router;
